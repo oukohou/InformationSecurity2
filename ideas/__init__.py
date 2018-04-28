26becc35b30075ed500082ee2a5322ddd26fa212
oukohou <1432641855@qq.com>
2018-04-28 11:23:11 +0800

    Initial commit

---
+++ ideas/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__author__ = 'oukohou'
+__time__ = '2017/11/16 10:56'
+
+# If this runs wrong, don't ask me, I don't know why;
+# If this runs right, thank god, and I don't know why.
+# Maybe the answer, my friend, is blowing in the wind.
